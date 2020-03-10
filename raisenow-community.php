a19ef9da94e0a41bdc8b57d9c187fb33580f0093
Christoph Arndt <christoph.arndt@greenpeace.org>
2020-03-10 23:18:18 +0100

    Version number update

--- raisenow-community.php
+++ raisenow-community.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/cyrillbolliger
  * Text Domain:     raisenow-community
  * Domain Path:     /languages
- * Version:         1.1.4
+ * Version:         1.2.0
  *
  * @package         Raisenow_Community
  */
